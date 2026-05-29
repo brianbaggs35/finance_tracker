c0691d25dc60467c46e91de21b75912829445ee8
Brian Baggs <brianbaggs@hotmail.com>
2026-05-29 03:40:29 -0300

    FT-1 Add user and refresh_token models

--- finance_tracker_api/app/models/user.rb
+++ finance_tracker_api/app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :transactions, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
 end
